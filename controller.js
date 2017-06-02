87dd8d5830f212a2623eae07239170adcbff1c90
Jonathan Lowell <elderlowell@gmail.com>
2017-06-01 19:26:21 -0600

    finished mini-userProfiles

--- controller.js
+++ controller.js
@@ -1,5 +1,13 @@
-angular.module('userProfiles').controller('MainController', function($scope){
-    
-    $scope.thisAppIsBroken = "This angular app is working";
-   
+angular.module('userProfiles').controller('MainController', function($scope, mainService){
+
+  $scope.thisAppIsBroken = "This angular app is working";
+
+  getUsers();
+
+  function getUsers() {
+    $scope.users = mainService.getUsers();
+  }
+
+  $scope.toggleFavorite = mainService.toggleFavorite;
+
 })
